bed5ab00b8d4e66b0cae2f49d5de2a7ba3b70da9
Guilherme Solinscki <guilherme.slnk@gmail.com>
2016-12-15 18:04:24 -0200

    Fixed pipelines menu and code style

--- pipemon.go
+++ pipemon.go
@@ -1,136 +1,169 @@
 package main
 
 import (
-    "fmt"
-    "strings"
-    "time"
-    "os"
-    "os/exec"
-    "database/sql"
-    "encoding/json"
-    "github.com/fatih/color"
-    _ "github.com/lib/pq"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/fatih/color"
+	_ "github.com/lib/pq"
+	"github.com/pipemon/models"
 )
 
-var db_jerico *sql.DB
-var db_jis *sql.DB
-var db_jps *sql.DB
+const (
+	jisPipelineName = "JIS"
+	jpsPipelineName = "JPS"
+
+	dbUser       = "postgres"
+	jericoDbName = "cloudification_development"
+	jisDbName    = "cloudification_jis_development"
+	jpsDbName    = "cloudification_jps_development"
+
+	reloadInterval = 5
+)
+
+var (
+	dbJerico *sql.DB
+	dbJIS    *sql.DB
+	dbJPS    *sql.DB
+)
 
 func main() {
-    db_jerico, err := sql.Open("postgres", "user=postgres dbname=cloudification_development sslmode=disable")
-    checkErr(err)
+	dbJerico = models.InitDB(fmt.Sprintf("user=%s dbname=%s sslmode=disable", dbUser, jericoDbName))
+	dbJIS = models.InitDB(fmt.Sprintf("user=%s dbname=%s sslmode=disable", dbUser, jisDbName))
+	dbJPS = models.InitDB(fmt.Sprintf("user=%s dbname=%s sslmode=disable", dbUser, jpsDbName))
 
-    db_jis, err = sql.Open("postgres", "user=postgres dbname=cloudification_jis_development sslmode=disable")
-    checkErr(err)
-
-    db_jps, err = sql.Open("postgres", "user=postgres dbname=cloudification_jps_development sslmode=disable")
-    checkErr(err)
-
-    for range time.Tick(time.Second *5) {
-        go queryPipelines(db_jerico)
-    }
+	listPipelines(dbJerico)
 }
 
-func queryPipelines(db *sql.DB) {
-    clearScr()
+func listPipelines(db *sql.DB) {
+	pipelines, err := models.QueryPipelines(db)
+	checkErr(err)
 
-    timestamp := time.Now().Local()
-    fmt.Println(timestamp.String())
+	clearScr()
+	fmt.Println(time.Now().Local().String())
 
-    rows, err := db.Query("SELECT id, state from pipelines ORDER BY id")
-    checkErr(err)
+	color.Yellow("### PIPEMON ###")
 
-    for rows.Next() {
-        var id int
-        var state string
+	for _, pipeline := range pipelines {
+		printID(pipeline.Id)
+		printSeparator()
+		fmt.Printf(pipeline.Type)
+		printSeparator()
+		printState(pipeline.State)
+		fmt.Printf("\n")
+	}
 
-        err = rows.Scan(&id, &state)
-        checkErr(err)
+	fmt.Print("Pipeline to query: ")
 
-        fmt.Println("%v", id)
-        queryPipelineSteps(id, db, "")
-    }
+	var input int
+	_, err = fmt.Scanf("%v\n", &input)
+	checkErr(err)
 
+	ticker := time.NewTicker(time.Second * reloadInterval)
+
+	for range ticker.C {
+		go showPipelineDetails(input, dbJerico)
+	}
 }
 
-func queryPipelineSteps(pipelineId int, db *sql.DB, padding string) {
-    steps, err := db.Query("SELECT id, step_class, state, async_result from pipeline_steps WHERE pipeline_id = $1 ORDER BY id", pipelineId)
+func showPipelineDetails(pipelineID int, db *sql.DB) {
+	clearScr()
 
-    for steps.Next() {
-        var id int
-        var step_class string
-        var state string
-        var async_result string
-        var async_result_data map[string]interface{}
+	fmt.Println(time.Now().Local().String())
+	fmt.Printf("PIPELINE: %v", pipelineID)
+	fmt.Printf("\n")
 
-        steps.Scan(&id, &step_class, &state, &async_result)
-        checkErr(err)
-
-        fmt.Printf(padding)
-        printId(id)
-        printSeparator()
-        printStep(step_class)
-        printSeparator()
-        printState(state)
-        fmt.Printf("\n")
-
-        json.Unmarshal([]byte(async_result), &async_result_data)
-        checkErr(err)
-
-        if async_result_data["external_pipeline_id"] != nil {
-            var external_pipeline_id float64
-            var external_pipeline_name string
-
-            external_pipeline_id = async_result_data["external_pipeline_id"].(float64)
-            external_pipeline_name = async_result_data["external_pipeline_name"].(string)
-
-            if external_pipeline_name == "JIS" {
-                queryPipelineSteps(int(external_pipeline_id), db_jis, "     |")
-            } else if external_pipeline_name == "JPS" {
-               queryPipelineSteps(int(external_pipeline_id), db_jps, "     |")
-            }
-        }
-    }
+	listPipelineSteps(pipelineID, db, 0)
 }
 
-func printId(id int) {
-    white := color.New(color.FgWhite).PrintfFunc()
-    white("%4v", id)
+func listPipelineSteps(pipelineID int, db *sql.DB, paddingLength int) {
+	steps, err := models.QueryPipelineSteps(pipelineID, db)
+	checkErr(err)
+
+	for _, step := range steps {
+		printPipelineStep(step, paddingLength)
+
+		json.Unmarshal([]byte(step.AsyncResult), &step.AsyncResultData)
+		checkErr(err)
+
+		if step.AsyncResultData["external_pipeline_id"] != nil {
+			externalPipelineID := step.AsyncResultData["external_pipeline_id"].(float64)
+			externalPipelineName := step.AsyncResultData["external_pipeline_name"].(string)
+
+			if externalPipelineName == jisPipelineName {
+				listPipelineSteps(int(externalPipelineID), dbJIS, 5)
+			} else if externalPipelineName == jpsPipelineName {
+				listPipelineSteps(int(externalPipelineID), dbJPS, 5)
+			}
+		}
+	}
 }
 
-func printStep(step_class string) {
-    blue := color.New(color.FgBlue).PrintfFunc()
-    blue("%-65s", step_class)
+func printPipelineStep(step *models.PipelineStep, paddingLength int) {
+	printPadding(paddingLength)
+	printID(step.Id)
+	printSeparator()
+	printStep(step.StepClass)
+	printSeparator()
+	printState(step.State)
+	fmt.Printf("\n")
+}
+
+func printPadding(length int) {
+	if length <= 0 {
+		return
+	}
+
+	for i := 0; i < length; i++ {
+		fmt.Printf(" ")
+	}
+
+	fmt.Printf("|")
+}
+
+func printID(id int) {
+	white := color.New(color.FgWhite).PrintfFunc()
+	white("%4v", id)
+}
+
+func printStep(stepClass string) {
+	blue := color.New(color.FgBlue).PrintfFunc()
+	blue("%-65s", stepClass)
 }
 
 func printSeparator() {
-    fmt.Printf(" | ")
+	fmt.Printf(" | ")
 }
 
 func printState(state string) {
-    red := color.New(color.FgRed).PrintfFunc()
-    green := color.New(color.FgGreen).PrintfFunc()
-    cyan := color.New(color.FgCyan).PrintfFunc()
+	red := color.New(color.FgRed).PrintfFunc()
+	green := color.New(color.FgGreen).PrintfFunc()
+	cyan := color.New(color.FgCyan).PrintfFunc()
 
-    if state == "running" {
-        cyan("%v", strings.ToUpper(state))
-    } else if state == "failed" {
-        red("%v", strings.ToUpper(state))
-    } else if state == "done" {
-        green("%v", strings.ToUpper(state))
-    } else {
-        fmt.Printf("PENDING")
-    }
-}
-
-func checkErr(err error) {
-    if err != nil {
-        panic(err)
-    }
+	if state == "running" {
+		cyan("%v", strings.ToUpper(state))
+	} else if state == "failed" {
+		red("%v", strings.ToUpper(state))
+	} else if state == "done" {
+		green("%v", strings.ToUpper(state))
+	} else {
+		fmt.Printf("PENDING")
+	}
 }
 
 func clearScr() {
-    cmd := exec.Command("clear")
-    cmd.Stdout = os.Stdout
-    cmd.Run()
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
